3042f1db58703309eb2624e3eddfdc48136f3265
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-03 10:08:21 -0800

    Done try django 1.8

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render
 from .forms import SignUpForm, ContactForm
 
+from .models import SignUp
 # Create your views here.
 
 def home(request):
@@ -30,7 +31,22 @@ def home(request):
 		instance.save()
 		context = {
 			"title": "Thank you",
-		}	
+		}
+
+	if request.user.is_authenticated() and request.user.is_staff:
+		#print(SignUp.objects.all())
+		# i = 1
+		# for instance in SignUp.objects.all():
+		# 	print(i)
+		# 	print(instance.full_name)
+		# 	i += 1
+
+		queryset = SignUp.objects.all().order_by('-timestamp') #.filter(full_name__iexact="Justin")
+		#print(SignUp.objects.all().order_by('-timestamp').filter(full_name__iexact="Justin").count())
+		context = {
+			"queryset": queryset
+		}
+
 	return render(request, "home.html", context)
 
 def contact(request):
